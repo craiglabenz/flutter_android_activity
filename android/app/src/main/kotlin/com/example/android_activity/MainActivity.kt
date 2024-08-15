34d010d197f1b12b8b4fc7c93dd6494cc064e8c9
Craig Labenz <craig.labenz@gmail.com>
2024-08-15 13:44:04 -0500

    added initial activity

--- android/app/src/main/kotlin/com/example/android_activity/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/android_activity/MainActivity.kt
@@ -1,5 +1,33 @@
 package com.example.android_activity
 
+import android.content.Intent
+import androidx.activity.ComponentActivity
 import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.engine.FlutterEngine
+import io.flutter.plugin.common.MethodCall
+import io.flutter.plugin.common.MethodChannel
+import io.flutter.plugins.GeneratedPluginRegistrant
 
-class MainActivity: FlutterActivity()
+class MainActivity: FlutterActivity() {
+    private val CHANNEL = "nativeChannel"
+
+    override fun configureFlutterEngine(flutterEngine: FlutterEngine) {
+        GeneratedPluginRegistrant.registerWith(flutterEngine)
+
+        MethodChannel(flutterEngine.dartExecutor.binaryMessenger, CHANNEL).setMethodCallHandler {
+            call: MethodCall, result: MethodChannel.Result ->
+                when (call.method) {
+                    "launchActivity" -> {
+                        try{
+                            val intent = Intent(this@MainActivity, SecondActivity::class.java)
+                            startActivity(intent)
+                        } catch (e: Exception){}
+                            result.success(true)
+                        }
+                        else -> {}
+                }
+        }
+    }
+}
+
+class SecondActivity : ComponentActivity()
